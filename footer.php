aab67bc5501e242845fbe50dc54f7eeb0db95d51
Esmond <esmondmccain@gmail.com>
2024-10-05 00:06:45 -0500

    added footer widgets to template

--- footer.php
+++ footer.php
@@ -8,37 +8,30 @@
  *
  * @package website-theme-name
  */
-
 ?>
+ <footer id="colophon" class="site-footer emThemefooter">
+<?php
+ if ( is_active_sidebar( 'footer-one' ) || is_active_sidebar( 'footer-two' ) || is_active_sidebar( 'footer-three' ) ) : ?>
+	<div class="footer-widgets">
+		<?php if ( is_active_sidebar( 'footer-one' ) ) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar( 'footer-one' ); ?>
+			</div>
+		<?php endif; ?>
+		<?php if ( is_active_sidebar( 'footer-two' ) ) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar( 'footer-two' ); ?>
+			</div>
+		<?php endif; ?>
+		<?php if ( is_active_sidebar( 'footer-three' ) ) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar( 'footer-three' ); ?>
+			</div>
+		<?php endif; ?>
+	</div><!-- .footer-widgets -->
+<?php endif; ?>
 
-<footer id="colophon" class="site-footer emThemefooter">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4 col-sm-4 col-xs-12">
-					<section><div class="contact-location"><div class="hours-title"><h4>location</h4></div><div class="address">9702 Greenwell Springs Rd</div><div class="city-state">Baton Rouge, LA</div><div class="zip">70814</div></div></section></div><div class="col-md-4 col-sm-4 col-xs-12"><section><div class="hours"><div class="hours-title"><h4>hours</h4></div><div class="days">
-                        Mon - Sun                    </div><div style="position:relative;left:-6px;"class="from-to">
-                        11:00 AM - 11:00 PM                    </div></div></section>
-					</div>
-					<div class="col-md-4 col-sm-4 col-xs-12">
-			<div class="find-us">
-			<section>
-				<div class="social">
-					<div class="social-title"><h4>find us on...</h4></div>
-					<div class="social-links social-icons-holder social-square">
-						<a title="emTheme Instagram" class="contact-icon" href="https://www.instagram.com/emThemebilliards/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-						<a title="emTheme Facebook" class="contact-icon" href="https://facebook.com/emThemebilliards/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-						<a title="emTheme Waitr" class="emThemewaitr-footer-contain" href="https://waitrapp.com/restaurants/la/baton-rouge/emTheme/18643" target="_blank"><span class="icon-emTheme-waitr-logo"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span></a></div>
-				</div>
-			</section>
-			<section>
-
-			<div class="contact-us"><div class="contact-us-title"><h4>contact us</h4></div>
-			<a title="emTheme Phone" href="tel:+12252566994" id="contact_us_v3_section_phone_link">(225)-256-6994</a><p>
-			<a title="emTheme Email" href="mailto:comments@emTheme.com">comments@emTheme.com</a></p></div>
-			</section></div>
-		</div>
-	</div>
-</div>
+<div class="copy-right">Copyright <?php echo date("Y"); ?></div>
 </footer><!-- #colophon -->
 <?php wp_footer(); ?>
 </body>
