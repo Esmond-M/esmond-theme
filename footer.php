b849049929eb4e74445c681fcf797922f39e7deb
Esmond <esmondmccain@gmail.com>
2024-10-04 19:17:28 -0500

    format changes

--- footer.php
+++ footer.php
@@ -12,19 +12,34 @@
 ?>
 
 <footer id="colophon" class="site-footer emThemefooter">
-		<div class="container"><div class="row"><div class="col-md-4 col-sm-4 col-xs-12"><section><div class="contact-location"><div class="hours-title"><h4>location</h4></div><div class="address">9702 Greenwell Springs Rd</div><div class="city-state">Baton Rouge, LA</div><div class="zip">70814</div></div></section></div><div class="col-md-4 col-sm-4 col-xs-12"><section><div class="hours"><div class="hours-title"><h4>hours</h4></div><div class="days">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-4 col-xs-12">
+					<section><div class="contact-location"><div class="hours-title"><h4>location</h4></div><div class="address">9702 Greenwell Springs Rd</div><div class="city-state">Baton Rouge, LA</div><div class="zip">70814</div></div></section></div><div class="col-md-4 col-sm-4 col-xs-12"><section><div class="hours"><div class="hours-title"><h4>hours</h4></div><div class="days">
                         Mon - Sun                    </div><div style="position:relative;left:-6px;"class="from-to">
-                        11:00 AM - 11:00 PM                    </div></div></section></div><div class="col-md-4 col-sm-4 col-xs-12">
-			<div class="find-us"><section>
-				<div class="social"><div class="social-title"><h4>find us on...</h4></div>
+                        11:00 AM - 11:00 PM                    </div></div></section>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12">
+			<div class="find-us">
+			<section>
+				<div class="social">
+					<div class="social-title"><h4>find us on...</h4></div>
 					<div class="social-links social-icons-holder social-square">
 						<a title="emTheme Instagram" class="contact-icon" href="https://www.instagram.com/emThemebilliards/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 						<a title="emTheme Facebook" class="contact-icon" href="https://facebook.com/emThemebilliards/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
 						<a title="emTheme Waitr" class="emThemewaitr-footer-contain" href="https://waitrapp.com/restaurants/la/baton-rouge/emTheme/18643" target="_blank"><span class="icon-emTheme-waitr-logo"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span></a></div>
-				</div></section>
-				<section><div class="contact-us"><div class="contact-us-title"><h4>contact us</h4></div>
-					<a title="emTheme Phone" href="tel:+12252566994" id="contact_us_v3_section_phone_link">(225)-256-6994</a><p>
-					<a title="emTheme Email" href="mailto:comments@emTheme.com">comments@emTheme.com</a></p></div></section></div></div></div></div></footer><!-- #colophon -->
+				</div>
+			</section>
+			<section>
+
+			<div class="contact-us"><div class="contact-us-title"><h4>contact us</h4></div>
+			<a title="emTheme Phone" href="tel:+12252566994" id="contact_us_v3_section_phone_link">(225)-256-6994</a><p>
+			<a title="emTheme Email" href="mailto:comments@emTheme.com">comments@emTheme.com</a></p></div>
+			</section></div>
+		</div>
+	</div>
+</div>
+</footer><!-- #colophon -->
 <?php wp_footer(); ?>
 </body>
 </html>
