d5ae8679501b4cd5fba4979d414dae04fbd1553d
Esmond <esmondmccain@gmail.com>
2024-11-16 13:22:00 -0600

    remove sass
    Want to make this on my own

--- header.php
+++ header.php
@@ -43,8 +43,23 @@
 		<?php 
 			wp_nav_menu(
 				array(
+					'container'            => '',
+					'container_class'      => '',
+					'container_id'         => '',
+					'container_aria_label' => '',
+					'menu_class'           => 'menu',
+					'menu_id'              => 'primary-menu',
+					'echo'                 => true,
+					'fallback_cb'          => 'wp_page_menu',
+					'before'               => '',
+					'after'                => '',
+					'link_before'          => '',
+					'link_after'           => '',
+					'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'item_spacing'         => 'preserve',
+					'depth'                => 0,
+					'walker'               => '',
 					'theme_location' => 'primary',
-					'menu_id'        => 'primary-menu',
 				)
 			);
 			?>
@@ -56,8 +71,23 @@
 			<?php 
 			wp_nav_menu(
 				array(
+					'container'            => '',
+					'container_class'      => '',
+					'container_id'         => '',
+					'container_aria_label' => '',
+					'menu_class'           => 'menu',
+					'menu_id'              => 'primary-menu',
+					'echo'                 => true,
+					'fallback_cb'          => 'wp_page_menu',
+					'before'               => '',
+					'after'                => '',
+					'link_before'          => '',
+					'link_after'           => '',
+					'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'item_spacing'         => 'preserve',
+					'depth'                => 0,
+					'walker'               => '',
 					'theme_location' => 'primary',
-					'menu_id'        => 'mobile-menu',
 				)
 			);
 			?>
