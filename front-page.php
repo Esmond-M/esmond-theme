aee791a6b1a9523110bd367550e40e64e4555a16
Esmond <esmondmccain@gmail.com>
2025-01-13 03:12:02 -0600

    added about and navigation menu
    initial content for about and navagation

--- front-page.php
+++ front-page.php
@@ -1,69 +1,86 @@
 <?php
-  
    get_header(); 
    
    ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class("em-page-content"); ?> >
-	
-<section>
-	<div class="entry-content">
-
-	</div><!-- .entry-content -->
-</section>
-<section id="em-portfolio-portfolio-section" class="em-portfolio-section">
-	
-<h2 id="portfolio" class="em-portfolio-section-title wow fadeInUp" data-wow-duration="0.2s" style="visibility: visible; animation-duration: 0.2s; animation-name: fadeInUp;">Portfolio</h2>
-
-</section>
-<section id="em-portfolio-featured-post-section" class="em-portfolio-section">
-   <div class="em-portfolio-container">
-   <h2 id="services" class="em-portfolio-section-title wow fadeInUp" data-wow-duration="0.5s" style="visibility: visible; animation-duration: 0.5s; animation-name: fadeInUp;">
-      Services
-   </h2>
-   <div class="em-portfolio-featured-post-wrap em-portfolio-clearfix">
-      <?php
-         $services_post_args = array(
-         	'post_type' => 'esmond-services',
-         	'post_status' => 'publish',
-         	'posts_per_page' => 3,
-         	'orderby'   => 'date',
-                'order' => 'ASC',
-         
-         );
-         
-         // Variable to call WP_Query.
-         $services_post_the_query = new WP_Query($services_post_args);
-         if ( $services_post_the_query->have_posts() ) :
-         	// Start the Loop
-         	while ( $services_post_the_query->have_posts() ) : $services_post_the_query->the_post();
-         		// Start the Loop
-         		$services_query_count = 1;
-         
-         			$title = get_the_title();
-         			$excerpt = get_the_excerpt();
-         			$post_id = get_the_ID();
-         			$meta = get_post_meta($post_id, 'services_post_icon_class_value', true);
-         			?>
-      <div class="em-portfolio-featured-post em-portfolio-featured-post<?php echo $services_query_count;?> wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 0s; animation-name: fadeInLeft;">
-         <div class="em-portfolio-featured-icon"><i class="<?php echo $meta; ?>"></i></div>
-         <h3><?php echo $title; ?></h3>
-         <div class="em-portfolio-featured-excerpt"><?php echo get_the_excerpt(); ?></div>
+   <section>
+      <div class="entry-content">
       </div>
-      <?php
-         $services_query_count + 1;
-         endwhile;
-         else:
-         // If no posts match this query, output this text.
-         	_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-         endif; 
-         
-         // If no posts match this query, output this text. 
-         wp_reset_postdata();
-         
-         ?> 
-   </div>
-</section>
-
-	
+      <!-- .entry-content -->
+   </section>
+   <section id="em-portfolio-home-slider-section">
+      <div id="em-portfolio-inner-contain">
+      </div>
+   </section>
+   <section class="em-portfolio-about-us-section">
+      <div class="upwork-btn-area">
+         <a target="_blank" href="https://www.upwork.com/o/profiles/users/_~01bc262783a150ad5c/">
+         <button class="upwork-btn">upwork reviews</button>		</a>
+      </div>
+      <h1 class="em-name"><b>Esmond MCcain</b></h1>
+      <h4 class="em-site-font-color">Full-stack Web Developer</h4>
+      <ul class="em-social-icons">
+         <li>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/esmond-m-a17244129/"><i class="fab fa-linkedin"></i></a>
+         </li>
+         <li>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.upwork.com/o/profiles/users/_~01bc262783a150ad5c/"><img class="upwork-svg-icon" src="<?php echo get_stylesheet_directory_uri() ?>\img\svg\upwork.svg" alt="Esmond Upwork Portfolio"></a>
+         </li>
+         <li>
+            <a rel="noopener noreferrer" target="_blank" href="https://github.com/Esmond-M"><i class="fab fa-github"></i></a>
+         </li>
+      </ul>
+   </section>
+   <section id="em-portfolio-portfolio-section" class="em-portfolio-section">
+      <h2 id="portfolio" class="em-portfolio-section-title wow fadeInUp" data-wow-duration="0.2s" style="visibility: visible; animation-duration: 0.2s; animation-name: fadeInUp;">Portfolio</h2>
+   </section>
+   <section id="em-portfolio-featured-post-section" class="em-portfolio-section">
+      <div class="em-portfolio-container">
+      <h2 id="services" class="em-portfolio-section-title wow fadeInUp" data-wow-duration="0.5s" style="visibility: visible; animation-duration: 0.5s; animation-name: fadeInUp;">
+         Services
+      </h2>
+      <div class="em-portfolio-featured-post-wrap em-portfolio-clearfix">
+         <?php
+            $services_post_args = array(
+            	'post_type' => 'esmond-services',
+            	'post_status' => 'publish',
+            	'posts_per_page' => 3,
+            	'orderby'   => 'date',
+                   'order' => 'ASC',
+            
+            );
+            
+            // Variable to call WP_Query.
+            $services_post_the_query = new WP_Query($services_post_args);
+            if ( $services_post_the_query->have_posts() ) :
+            	// Start the Loop
+            	while ( $services_post_the_query->have_posts() ) : $services_post_the_query->the_post();
+            		// Start the Loop
+            		$services_query_count = 1;
+            
+            			$title = get_the_title();
+            			$excerpt = get_the_excerpt();
+            			$post_id = get_the_ID();
+            			$meta = get_post_meta($post_id, 'services_post_icon_class_value', true);
+            			?>
+         <div class="em-portfolio-featured-post em-portfolio-featured-post<?php echo $services_query_count;?> wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 0s; animation-name: fadeInLeft;">
+            <div class="em-portfolio-featured-icon"><i class="<?php echo $meta; ?>"></i></div>
+            <h3><?php echo $title; ?></h3>
+            <div class="em-portfolio-featured-excerpt"><?php echo get_the_excerpt(); ?></div>
+         </div>
+         <?php
+            $services_query_count + 1;
+            endwhile;
+            else:
+            // If no posts match this query, output this text.
+            	_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+            endif; 
+            
+            // If no posts match this query, output this text. 
+            wp_reset_postdata();
+            
+            ?> 
+      </div>
+   </section>
 </article>
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -25,24 +25,10 @@
 <div id="page" class="site">
 
 	<header id="masthead" class="site-header">
-	<div class="site-branding">
-		 <?php       
-
-     $custom_logo_id = get_theme_mod( 'custom_logo' );
-     $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-     if ( has_custom_logo() ) {
-           $custom_logo_image = esc_url( $logo[0] );
-     } else {
-            $custom_logo_image = get_template_directory_uri() . '/img/logo-placeholder-image.png';
-     }
-     ?>
-     <a href="<?php echo home_url(); ?>"><img class="site-branding-img" src="<?php echo  $custom_logo_image; ?>" alt="Logo" class="logo-img"></a> 
-			
-		
-		</div><!-- .site-branding -->
 		<?php 
 			wp_nav_menu(
 				array(
+					'menu'            =>17,
 					'container'            => '',
 					'container_class'      => '',
 					'container_id'         => '',
@@ -63,33 +49,4 @@
 				)
 			);
 			?>
-	
-
-	<button class="emTheme-openbtn"><i class="fas fa-bars"></i> Menu</button>
-		<div id="mySidenav" class="sidenav">
-			<a href="javascript:void(0)" class="emTheme-closebtn">Close <i class="fa fa-times" aria-hidden="true"></i></a>
-			<?php 
-			wp_nav_menu(
-				array(
-					'container'            => '',
-					'container_class'      => '',
-					'container_id'         => '',
-					'container_aria_label' => '',
-					'menu_class'           => 'menu',
-					'menu_id'              => 'primary-menu',
-					'echo'                 => true,
-					'fallback_cb'          => 'wp_page_menu',
-					'before'               => '',
-					'after'                => '',
-					'link_before'          => '',
-					'link_after'           => '',
-					'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-					'item_spacing'         => 'preserve',
-					'depth'                => 0,
-					'walker'               => '',
-					'theme_location' => 'primary',
-				)
-			);
-			?>
-
 	</header><!-- #masthead -->
