d92cf6c5914a5977dbc66a3fefe666fd8a6c9e29
Esmond <esmondmccain@gmail.com>
2025-08-08 20:41:27 -0500

    made header more semantic

--- header.php
+++ header.php
@@ -2,51 +2,39 @@
 /**
  * The header for our theme
  *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
+ * Displays the <head> section and everything up until <main>.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  * @package esmond-theme-portfolio
  */
-
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-<div class="emTheme-fixedheader-placeholder"></div>
-	<header id="masthead" class="site-header">
-		<?php 
-			wp_nav_menu(
-				array(
-					'menu'                 => 8,
-					'container'            => 'ul',
-					'container_class'      => '',
-					'container_id'         => '',
-					'container_aria_label' => '',
-					'menu_class'           => 'menu',
-					'menu_id'              => 'primary-menu',
-					'echo'                 => true,
-					'fallback_cb'          => 'wp_page_menu',
-					'before'               => '',
-					'after'                => '',
-					'link_before'          => '',
-					'link_after'           => '',
-					'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-					'item_spacing'         => 'preserve',
-					'depth'                => 0,
-					'walker'               => '',
-					'theme_location' => 'primary_menu',
-				)
-			);
-			?>
-	</header><!-- #masthead -->
+    <div class="emTheme-fixedheader-placeholder" aria-hidden="true"></div>
+    <header id="masthead" class="site-header" role="banner">
+        <nav id="site-navigation" class="main-navigation" aria-label="Primary Menu">
+            <?php 
+                wp_nav_menu(
+                    array(
+                        'menu'                 => 8,
+                        'container'            => false,
+                        'menu_class'           => 'menu',
+                        'menu_id'              => 'primary-menu',
+                        'echo'                 => true,
+                        'fallback_cb'          => 'wp_page_menu',
+                        'items_wrap'           => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                        'theme_location'       => 'primary_menu',
+                    )
+                );
+            ?>
+        </nav>
+    </header><!-- #masthead -->
