c8429b0cb718889e8c16a5d0674eaeb7886b4735
Esmond <esmondmccain@gmail.com>
2025-08-14 00:36:01 -0500

    styled hero image for 404

--- 404.php
+++ 404.php
@@ -2,12 +2,11 @@
    get_header('404'); 
    ?>
 <main id="post-<?php the_ID(); ?>" <?php post_class('em-page-content'); ?> aria-labelledby="em-404-title">
-   <section class="em-404-hero" style="background: url('<?php echo esc_url( get_stylesheet_directory_uri() . '/img/bg.jpg' ); ?>') center center/cover no-repeat; min-height: 400px; display: flex; flex-direction: column; justify-content: center; align-items: center; color: #fff;">
+   <section class="em-404-hero em-404-hero-style" style="background: url('<?php echo esc_url( get_stylesheet_directory_uri() . '/img/bg.jpg' ); ?>') center center/cover no-repeat;">
       <h1 id="em-404-title" class="em-name"><b>Oops! Page Not Found (404)</b></h1>
-      <p class="em-site-font-color">Sorry, the page you are looking for does not exist or has been moved.</p>
+      <p>Sorry, the page you are looking for does not exist or has been moved.</p>
       <div class="em-404-actions">
          <a href="/" class="upwork-btn">Return Home</a>
-         <a href="javascript:history.back()" class="upwork-btn">Go Back</a>
       </div>
    </section>
 </main>
