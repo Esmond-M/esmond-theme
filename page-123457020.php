1183fdf425f20e566ac7774fd976ab1ccc180c8d
Esmond <esmondmccain@gmail.com>
2026-03-05 21:46:02 -0600

    feat: deploy drag-and-drop task reordering build

--- page-123457020.php
+++ page-123457020.php
@@ -7,8 +7,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-    <script type="module" crossorigin src="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/project-manager-app/assets/index--0Png9aC.js"></script>
-    <link rel="stylesheet" crossorigin href="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/project-manager-app/assets/index-7Lt5Ngjq.css">
+    <script type="module" crossorigin src="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/project-manager-app/assets/index-CLJNWtf_.js"></script>
+    <link rel="stylesheet" crossorigin href="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/project-manager-app/assets/index-B8J4XLBb.css">
   </head>
   <body>
     <div id="root"></div>
