672495eab91873bd61058e1917b26b965e09ab9e
Esmond <esmondmccain@gmail.com>
2026-03-09 16:15:23 -0500

    Update page-123457036.php

--- page-123457036.php
+++ page-123457036.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-    <script type="module" crossorigin src="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/headless-dashboard-app/assets/index-UhU3cnQq.js"></script>
+    <script type="module" crossorigin src="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/headless-dashboard-app/assets/index-CICVorA_.js"></script>
     <link rel="stylesheet" crossorigin href="<?php echo get_stylesheet_directory_uri(); ?>/react-projects/headless-dashboard-app/assets/index-Xknimmyb.css">
   </head>
   <body>
